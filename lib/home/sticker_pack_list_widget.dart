87a99390edff6c8db9f72bd042c0b77a6f847e53
Chiggy-Playz <garg.chirag2004@gmail.com>
2023-11-13 03:41:05 +0530

    Redesign Homepage

--- lib/home/sticker_pack_list_widget.dart
+++ lib/home/sticker_pack_list_widget.dart
@@ -2,6 +2,7 @@ import 'package:assets_repository/assets_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mime_app/core/widgets/empty_widget.dart';
+import 'package:mime_app/home/sticker_pack_preview_widget.dart';
 import 'package:mime_app/home/sticker_pack_widget.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:skeletonizer/skeletonizer.dart';
@@ -43,29 +44,26 @@ class StickerPackListWidgetState extends State<StickerPackListWidget> {
           ),
           clipBehavior: Clip.antiAlias,
           elevation: 8,
-          child: Theme(
-            data: Theme.of(context).copyWith(
-              dividerColor: Colors.transparent,
-              expansionTileTheme: ExpansionTileThemeData(
-                tilePadding: EdgeInsets.symmetric(horizontal: 4.w),
-                childrenPadding: EdgeInsets.symmetric(horizontal: 4.w),
-              ),
-            ),
-            child: ExpansionTile(
-                title: Skeleton.keep(
-                  keep: widget.packsLoaded[pack]!,
-                  child: Text(pack.name,
-                      style: Theme.of(context).textTheme.bodyLarge),
-                ),
-                initiallyExpanded: true,
-                children: [
-                  Skeleton.keep(
+          child: InkWell(
+            onTap: () {},
+            child: Column(
+              children: [
+                ListTile(
+                  title: Skeleton.keep(
                     keep: widget.packsLoaded[pack]!,
-                    child: StickerPackWidget(
-                      pack: pack,
-                    ),
+                    child: Text(pack.name,
+                        style: Theme.of(context).textTheme.bodyLarge),
                   ),
-                ]),
+                  subtitle: Text("${pack.assets.length} stickers"),
+                ),
+                Skeleton.keep(
+                  keep: widget.packsLoaded[pack]!,
+                  child: StickerPackPreviewWidget(
+                    pack: pack,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
@@ -79,34 +77,27 @@ class StickerPackListWidgetState extends State<StickerPackListWidget> {
       },
       child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 2.w, vertical: 1.h),
-          child: ListTileTheme(
-            contentPadding: const EdgeInsets.all(0),
-            dense: true,
-            horizontalTitleGap: 0.0,
-            minVerticalPadding: 0,
-            minLeadingWidth: 0,
-            child: widget.packs.isEmpty
-                ? const CustomScrollView(
-                    physics: AlwaysScrollableScrollPhysics(),
-                    slivers: [
-                        SliverFillRemaining(
-                          child: EmptyWidget(
-                              title: "No stickers yet",
-                              icon: Icons.image_not_supported,
-                              description:
-                                  "You don't have any stickers yet. Get started by creating a new pack or importing from discord!"),
-                        ),
-                      ])
-                : ListView.builder(
-                    primary: false,
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    key: UniqueKey(),
-                    itemCount: widget.packs.length,
-                    itemBuilder: (context, index) {
-                      return expansionTiles[index];
-                    },
-                  ),
-          )),
+          child: widget.packs.isEmpty
+              ? const CustomScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  slivers: [
+                      SliverFillRemaining(
+                        child: EmptyWidget(
+                            title: "No stickers yet",
+                            icon: Icons.image_not_supported,
+                            description:
+                                "You don't have any stickers yet. Get started by creating a new pack or importing from discord!"),
+                      ),
+                    ])
+              : ListView.builder(
+                  primary: false,
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  key: UniqueKey(),
+                  itemCount: widget.packs.length,
+                  itemBuilder: (context, index) {
+                    return expansionTiles[index];
+                  },
+                )),
     );
   }
 }
---
+++ lib/home/sticker_pack_preview_widget.dart
@@ -0,0 +1,102 @@
+import 'dart:io';
+
+import 'package:assets_repository/assets_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+class StickerPackPreviewWidget extends StatefulWidget {
+  const StickerPackPreviewWidget({super.key, required this.pack});
+
+  final Pack pack;
+
+  @override
+  State<StickerPackPreviewWidget> createState() =>
+      _StickerPackPreviewWidgetState();
+}
+
+class _StickerPackPreviewWidgetState extends State<StickerPackPreviewWidget> {
+  Directory? cacheDir;
+
+  @override
+  void initState() {
+    super.initState();
+    getApplicationCacheDirectory().then((value) => setState(() {
+          cacheDir = value;
+        }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 2.h, horizontal: 2.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // We'll show 3 preview stickers and 4th widget will indicate how many stickers are in the pack
+        children: [
+          for (var asset in widget.pack.assets.take(3))
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 1.w),
+              child: Expanded(
+                child: Skeleton.replace(
+                  height: 18.w,
+                  width: 18.w,
+                  replacement: const Skeleton.shade(child: Card()),
+                  child: cacheDir == null
+                      ? const Skeleton.shade(child: Card())
+                      : Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(12),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                blurRadius: 20,
+                                offset: const Offset(0, 10),
+                              ),
+                            ],
+                            image: DecorationImage(
+                              image: FileImage(asset.file(cacheDir!)),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          width: 18.w,
+                          height: 18.w,
+                        ),
+                ),
+              ),
+            ),
+          if (widget.pack.assets.length > 3)
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 1.w),
+              child: Expanded(
+                child: Skeleton.replace(
+                  height: 18.w,
+                  width: 18.w,
+                  replacement: const Skeleton.shade(child: Card()),
+                  child: cacheDir == null
+                      ? const Skeleton.shade(child: Card())
+                      : SizedBox(
+                          width: 18.w,
+                          height: 18.w,
+                          child: Center(
+                            child: Text(
+                              "+${widget.pack.assets.length - 3} More",
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyLarge
+                                  ?.copyWith(
+                                    color: Colors.white,
+                                  ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
